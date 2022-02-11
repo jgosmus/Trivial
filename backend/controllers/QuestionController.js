92e0f89982df8997d0032b674221defe3b9885eb
jgosmus <jgosmus@gmail.com>
2022-02-12 00:43:57 +0100

    Fix questionsScript and eslint

--- backend/controllers/QuestionController.js
+++ backend/controllers/QuestionController.js
@@ -1,9 +1,9 @@
 const getQuestion = async (req, res) => {
-    return res.status(200).send('Question to ask')
+    return res.json({ question: 'Question to ask' })
 }
 
 const save = async (req, res) => {
-    return res.status(200).send('Question to ask')
+    return res.json({ question: 'Question to ask' })
 }
 
 module.exports = { getQuestion, save }
